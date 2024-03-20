8d16cfcf5d534d1e9b3a43cbd77313025cd953aa
podestal <luisrodriguezpodesta@gmail.com>
2024-03-20 10:57:20 -0400

    create new assignment with fixed data

--- mobile/api/api.js
+++ mobile/api/api.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 
 const URL = 'http://127.0.0.1:8000/'
+const PROD_URL = ''
+
+// AUTH
 
 export const createUser = (userData) => axios.post(`${URL}auth/users/`, userData)
 
@@ -10,13 +13,23 @@ export const getUser = (token) => axios.get(`${URL}auth/users/me/`, {
     headers: { Authorization: `JWT ${token}` }
 })
 
+// ASSIGNATURES
+
 export const getAssignatures = token => axios.get(`${URL}api/assignatures/` ,{
     headers: { Authorization: `JWT ${token}` }
 })
 
+// ASIGNMENTS
+
 export const getAssignments = data => axios.get(`${URL}api/assignments/?assignature=${data.assignature}` ,{
     headers: { Authorization: `JWT ${data.token}` }
 })
+
+export const createAssignment = data => axios.post(`${URL}api/assignments/`, data.assignment ,{
+    headers: { Authorization: `JWT ${data.token}` }
+})
+
+// GRADES
 
 export const getGrades = data => axios.get(`${URL}api/assignments/${data.assignmentId}/grades/` ,{
     headers: { Authorization: `JWT ${data.token}` }
@@ -25,6 +38,8 @@ export const getGrades = data => axios.get(`${URL}api/assignments/${data.assignm
 export const updateGrades = data => axios.patch(`${URL}api/assignments/${data.assignmentId}/grades/${data.gradeId}/`,  data.calification,{
     headers: { Authorization: `JWT ${data.token}` }
 })
+
+// COMPETENCIES
 
 export const getCompetencies = data => axios.get(`${URL}api/competences/` ,{
     headers: { Authorization: `JWT ${data.token}` }
--- mobile/components/school/AssignmentForm.js
+++ mobile/components/school/AssignmentForm.js
@@ -5,6 +5,9 @@ import ButtonElement from "../utils/Button"
 import Input from "../utils/Input"
 import { useEffect, useState } from "react"
 import Title from "../utils/Title"
+import { createAssignment } from "../../api/api"
+import { useMutation } from "@tanstack/react-query"
+import useAuth from "../../hooks/useAuth"
 
 const AssignmentForm = ({ route }) => {
 
@@ -12,11 +15,30 @@ const AssignmentForm = ({ route }) => {
     const [title, setTitle] = useState('')
     const [dueDate, setDueDate] = useState('')
     const [competence, setCompetence] = useState('')
+    const assignatureId = route?.params?.assignatureId
+    const { user } = useAuth()
+
+    const {mutate: createAssignmentMutation} = useMutation({
+        mutationFn: data => createAssignment(data),
+        onSuccess: res => console.log(res.data),
+        onError: err => console.log(err)
+    })
 
     useEffect(() => {
         console.log('competencies from assignment form', competencies);
-        console.log('assignature Id', route.params.assignatureId);
     }, [])
+
+    const handleCreateAssignment = () => {
+        createAssignmentMutation({ 
+            token: user.access, 
+            assignment:{
+                title: 'New Assignment',
+                due_date: '2024-03-20',
+                competence: 4,
+                assignature: assignatureId,
+            } 
+        })
+    }
 
   return (
     <Container>
@@ -38,6 +60,7 @@ const AssignmentForm = ({ route }) => {
         />
         <ButtonElement 
             title={'Crear'}
+            onPress={handleCreateAssignment}
         />
     </Container>
   )
