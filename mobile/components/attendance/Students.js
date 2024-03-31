99d4c4617aee4008c0c7d64258036896118c4a8b
podestal <luisrodriguezpodesta@gmail.com>
2024-03-31 18:47:16 -0500

    filter student by name and last name

--- mobile/components/attendance/Students.js
+++ mobile/components/attendance/Students.js
@@ -6,10 +6,13 @@ import { getStudentsBySchool } from "../../api/api"
 import useAuth from "../../hooks/useAuth"
 import List from "../utils/List"
 import Student from "./Student"
+import Input from "../utils/Input"
+import NonScrollableContainer from "../utils/NonScrollableContainer"
 
 const Students = ({ clases, schoolId }) => {
 
     const [clase, setClase] = useState('')
+    const [name, setName] = useState('')
     const { user } = useAuth()
     const {data: students, isLoading, isError, error} = useQuery({
         queryKey: ['studentsBySchool'],
@@ -21,22 +24,37 @@ const Students = ({ clases, schoolId }) => {
     if (isError) return <Text>{error.message}</Text>
 
   return (
-    <>
+    <NonScrollableContainer>
         {console.log('Clase selected',clase)}
         <Select 
             title={'Clase'}
             setter={setClase}
             data={clases}
         />
+        <Input
+            label={'Buscar...'}
+            value={name}
+            setter={setName}
+            placeholder={'Nombre o Apellido'}
+        />
         <List 
             // data, DetailComponent, style, extraData
             data={students.data
                     .filter(student => student.clase == clase)
+                    .filter(student => (
+                        student?.first_name.toLowerCase().includes(name.toLocaleLowerCase()) || 
+                        student?.last_name.toLowerCase().includes(name.toLocaleLowerCase())
+                    ))
                 }
             DetailComponent={Student}
         />
-    </>
+    </NonScrollableContainer>
   )
 }
+
+// .filter( grade => (
+//     grade?.student?.first_name.toLowerCase().includes(name.toLocaleLowerCase()) ||  
+//     grade?.student?.last_name.toLowerCase().includes(name.toLocaleLowerCase()))
+// )
 
 export default Students
