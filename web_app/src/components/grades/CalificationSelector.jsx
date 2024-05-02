1f7d73f31cedfd6b18e023eef311fe3f40ba5f73
podestal <luisrodriguezpodesta@gmail.com>
2024-05-02 12:58:51 -0400

    handling error ui

--- web_app/src/components/grades/CalificationSelector.jsx
+++ web_app/src/components/grades/CalificationSelector.jsx
@@ -3,10 +3,14 @@ import useAuth from "../../hooks/useAuth"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { updateGrades } from "../../api/api"
 import { useState } from "react"
+import { RiCheckLine, RiBugLine } from "@remixicon/react"
+import { Badge } from "@tremor/react"
 
 const CalificationSelector = ({ calification, setCalification, gradeId }) => {
 
   const [prevCalification, setPrevCalification] = useState(calification && calification || '')
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
   const {user} = useAuth()
   const queryClient = useQueryClient()
   const { mutate: updateGradesMutation } = useMutation({
@@ -14,27 +18,38 @@ const CalificationSelector = ({ calification, setCalification, gradeId }) => {
     onSuccess: res => {
       queryClient.invalidateQueries(['grades'])  
       setPrevCalification(calification)
+      setSuccess(true)
     },
-    onError: err => setCalification(prevCalification)
+    onError: err => {
+      setCalification(prevCalification)
+      setError(true)
+    }
 
   })
 
   const handleUpdate = () => {
     updateGradesMutation({ token: user.access, gradeId, calification: { calification }  })
+    setTimeout(() => {
+      setSuccess(false)
+      setError(false)
+    }, 2000)
   }
 
   return (
-    <SearchSelect className="w-[70px] mx-4" value={calification} 
-      onValueChange={ value => {
-        setCalification(value)
-        handleUpdate()
-      }}>
-        {console.log('calification from selector', calification)}
-        <SearchSelectItem value="AD">AD</SearchSelectItem>
-        <SearchSelectItem value="A">A</SearchSelectItem>
-        <SearchSelectItem value="B">B</SearchSelectItem>
-        <SearchSelectItem value="C">C</SearchSelectItem>
-    </SearchSelect>
+    <div className="flex flex-col justify-end items-start">
+      {success && <Badge className="mx-8 mb-6 w-[140px]" color='green' icon={RiCheckLine}>Nota Cambiada</Badge>} 
+      {error && <Badge className="mx-8 mb-6 w-[140px]" color='red' icon={RiBugLine}>Ocurrió un error</Badge>}  
+      <SearchSelect className="w-[70px] mx-4" value={calification} 
+        onValueChange={ value => {
+          setCalification(value)
+          handleUpdate()
+        }}>
+          <SearchSelectItem value="AD">AD</SearchSelectItem>
+          <SearchSelectItem value="A">A</SearchSelectItem>
+          <SearchSelectItem value="B">B</SearchSelectItem>
+          <SearchSelectItem value="C">C</SearchSelectItem>
+      </SearchSelect>
+    </div>
   )
 }
 
