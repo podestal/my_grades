a2a98ca53842e93129c928893c430fae60818ff1
podestal <luisrodriguezpodesta@gmail.com>
2024-05-06 12:28:20 -0400

    ultimate details to activty card

--- web_app/src/components/activities/ActivityCard.jsx
+++ web_app/src/components/activities/ActivityCard.jsx
@@ -17,19 +17,20 @@ const ActivityCard = ({ activity }) => {
   return (
     <div 
         onClick={() => navigate('/activity', { replace: true, state: activity})}
-        className='text-center font-bold mx-auto my-12 py-8 w-[300px] border border-purple-950  rounded-[35px] cursor-pointer hover:bg-purple-950 shadow-violet-900 shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]'
+        className='text-center h-60 font-bold mx-auto my-12 py-8 w-[300px] flex flex-col border border-purple-950  rounded-[35px] cursor-pointer hover:bg-purple-950 shadow-violet-900 shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]'
     >
-        <div className='flex items-center justify-between mx-6 gap-4 mb-4'>
+        <div className='flex items-start justify-between mx-6 gap-4 mb-4'>
             <h3 className="font-poppins text-2xl">{activity.title}</h3>
             <Badge color={color} icon={icon}></Badge>
         </div>
-        <i class="ri-progress-2-line"></i>
-        <p>Faltan calificar: {remainingToGrade}</p>
-        <div className="flex items-center justify-center gap-6 mt-6 ">
-            <p>AD: <span className="text-green-500">{filteredGrades.filter(grade => grade.calification == 'AD').length}</span></p>
-            <p>A: <span className="text-yellow-300">{filteredGrades.filter(grade => grade.calification == 'A').length}</span></p>
-            <p>B: <span className="text-amber-500">{filteredGrades.filter(grade => grade.calification == 'B').length}</span></p>
-            <p>C: <span className="text-red-500">{filteredGrades.filter(grade => grade.calification == 'C').length}</span></p>
+        <div className="flex flex-col items-center justify-end gap-4 mt-6 h-full">
+            <p>Faltan calificar: <span className="text-blue-500">{remainingToGrade}</span></p>
+            <div className="flex gap-5">
+                <p>AD: <span className="text-green-500">{filteredGrades.filter(grade => grade.calification == 'AD').length}</span></p>
+                <p>A: <span className="text-yellow-300">{filteredGrades.filter(grade => grade.calification == 'A').length}</span></p>
+                <p>B: <span className="text-amber-500">{filteredGrades.filter(grade => grade.calification == 'B').length}</span></p>
+                <p>C: <span className="text-red-500">{filteredGrades.filter(grade => grade.calification == 'C').length}</span></p>
+            </div>
         </div>
     </div>
   )
